f1836127a14e5dd7316d1d5c30ac790f7a6a82f1
Yash Patel <yasupatel219@gmail.com>
2025-09-16 15:19:25 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -51,7 +51,7 @@ app.get('/', (req, res) => {
 // Serve frontend in production (optional)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
