60614f361084a4d0657d6a94d8e4458c874025ec
Yash Patel <yasupatel219@gmail.com>
2025-09-16 15:02:56 +0530

    push

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://mock-ai-1.onrender.com',
   credentials: true,
 }));
 app.use(passport.initialize());
